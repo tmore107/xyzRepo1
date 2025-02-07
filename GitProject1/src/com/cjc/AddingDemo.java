cc99faea8a814f4973857bf21b831988e8078e4c
tmore107 <moretrupti107@gmail.com>
2025-02-07 09:42:13 +0530

    Git by Trupti

--- GitProject1/src/com/cjc/AddingDemo.java
+++ GitProject1/src/com/cjc/AddingDemo.java
@@ -4,6 +4,7 @@ public class AddingDemo {
 
 	public void m1(){
 		System.out.println("This is m1 method()");
+		System.out.println("Demo method");
 	}
 	public static void main(String[] args) {
 		System.out.println("This is main class");
