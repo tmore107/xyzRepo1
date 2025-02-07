9a3a7d2498796e5247a31fb9119122f4eed7b459
tmore107 <moretrupti107@gmail.com>
2025-02-07 09:41:31 +0530

    Update AddingDemo.java

--- GitProject1/src/com/cjc/AddingDemo.java
+++ GitProject1/src/com/cjc/AddingDemo.java
@@ -2,6 +2,9 @@ package com.cjc;
 
 public class AddingDemo {
 
+	public void m1(){
+		System.out.println("This is m1 method()");
+	}
 	public static void main(String[] args) {
 		System.out.println("This is main class");
 
